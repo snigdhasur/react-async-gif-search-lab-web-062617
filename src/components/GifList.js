33036f7a263218f5a48485e4e90fd5390add05cf
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-09-06 10:38:02 -0400

    iframe and img src options

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const GifList = (props) => {
 
-  const gifLIs = props.gifs.map((gif, index) => <li key={index}><img src={gif.images.original.url} ></img></li>)
+	// <li key={index}><img src={gif.images.original.url} ></img></li>
+	// image way above
+
+  const gifLIs = props.gifs.map((gif, index) => <li key={index}><iframe src={gif.embed_url} width="480" height="271" frameBorder="0" className="giphy-embed"></iframe></li>)
 
   return(
       <ul>
