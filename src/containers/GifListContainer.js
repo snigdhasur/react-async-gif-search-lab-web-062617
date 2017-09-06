a500a1336ac92dd2690c704ac0cefe5ef63d5d97
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-09-06 09:21:45 -0400

    added giflist container

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class GifListContainer extends React.Component {
+	constructor(){
+		super()
+
+		state = {
+
+		}
+	}
+
+	render(){
+		return(
+		)
+	}
+
+}
+
+export defautl GifListContainer
